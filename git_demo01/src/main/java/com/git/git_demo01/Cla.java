64e6922a066dc7f56c51186d6d9b472cf11c815f
hx196 <1027476945@qq.com>
2020-12-17 14:20:36 +0800

    本地上传

--- git_demo01/src/main/java/com/git/git_demo01/Cla.java
+++ git_demo01/src/main/java/com/git/git_demo01/Cla.java
@@ -4,5 +4,6 @@ public class Cla {
     public static void main(String[] args) {
         System.out.println("添加文件");
         System.out.println("克隆项目");
+        System.out.println("本地上传");
     }
 }
